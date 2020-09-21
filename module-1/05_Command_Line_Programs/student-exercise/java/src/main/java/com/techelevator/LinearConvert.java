ae774380f66ded5c1aded20db8b6e31638e34ba4
Brian Kilburn <briankilburn93@gmail.com>
2020-09-20 23:41:33 -0400

    Temp Convert and Linear Convert Complete

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,8 +1,33 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
+		
+		double convertedLength = 0;
+		String inFeetOrMeters = "";
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("Please enter the length:");
+		double length = userInput.nextDouble();
+		
+		userInput.nextLine();
+		
+		System.out.println("Is the length in (m)eter, or (f)eet?");
+		inFeetOrMeters = userInput.nextLine();
+		
+		if(inFeetOrMeters.contains("F") || inFeetOrMeters.contains("f"))
+		{
+			convertedLength = (length * 0.3048);
+			System.out.printf("%.2ff is %.2fm.",length, convertedLength);
+		}
+		else if(inFeetOrMeters.contains("C") || inFeetOrMeters.contains("c"))
+		{
+			convertedLength = (length * 3.2808399);
+			System.out.printf("%.2fm is %.2ff.",length, convertedLength);
+		}
 
 	}
 
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,34 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
 
+		double convertedTemp = 0;
+		String inFOrC = "";
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("Please enter the temperature:");
+		double temperature = userInput.nextDouble();
+		
+		userInput.nextLine();
+		
+		System.out.println("Is the temperature in (C)elsius, or (F)ahrenheit?");
+		inFOrC = userInput.nextLine();
+		
+		if(inFOrC.contains("F") || inFOrC.contains("f"))
+		{
+			convertedTemp = (temperature - 32) / 1.8;
+			System.out.printf("%.2fF is %.2fC.",temperature, convertedTemp);
+		}
+		else if(inFOrC.contains("C") || inFOrC.contains("c"))
+		{
+			convertedTemp = (temperature * 1.8) + 32;
+			System.out.printf("%.2fC is %.2fF.",temperature, convertedTemp);
+		}
+		
 	}
 
 }
