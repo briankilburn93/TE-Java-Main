4872d16f1276a9ed8cc0fab44ac9ac5e56c79fa2
Brian Kilburn <briankilburn93@gmail.com>
2020-11-19 18:14:20 -0500

    DOM HW complete

--- module-3/08_DOM/student-exercise/shopping-list/js/shopping-list.js
+++ module-3/08_DOM/student-exercise/shopping-list/js/shopping-list.js
@@ -1,23 +1,54 @@
 // add pageTitle
+const pageTitle = 'My Shopping List';
 
 // add groceries
+const groceries = [
+  "bananas",
+  "apples",
+  "grapes",
+  "water",
+  "coffee",
+  "tee",
+  "chicken",
+  "beef",
+  "turkey",
+  "ham"
+];
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  document.getElementById('title').innerText=pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  
+  groceries.forEach( (anItem) => {
+    const newItem = document.createElement('li');
+    newItem.setAttribute('class', 'item');
+    newItem.innerText = anItem;
+
+    document.getElementById('groceries').appendChild(newItem);
+  });
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  let theList = document.getElementById('groceries');
+  let itemList = Array.from(theList.children);
+
+itemList.forEach((listItems) => {
+  listItems.setAttribute('class', 'completed');
+});
+}
 
 setPageTitle();
 
--- module-3/09_Event_Handling/student-lecture/product-reviews/js/app.js
+++ module-3/09_Event_Handling/student-lecture/product-reviews/js/app.js
@@ -31,7 +31,9 @@ function setPageDescription() {
  * I will display all of the reviews in the reviews array
  */
 function displayReviews() {
+  // If the html content supports a template, add the template to the DOM
   if ('content' in document.createElement('template')) {
+    // now that we have the template i the DOM we have to add content
     reviews.forEach((review) => {
       displayReview(review);
     });
@@ -45,39 +47,109 @@ function displayReviews() {
  * @param {Object} review The review to display
  */
 function displayReview(review) {
+  // Get a reference to the element with id='main'
   const main = document.getElementById('main');
+  // Clone the template and get a reference to it
   const tmpl = document.getElementById('review-template').content.cloneNode(true);
-  tmpl.querySelector('h4').innerHTML = review.reviewer;
-  tmpl.querySelector('h3').innerHTML = review.title;
-  tmpl.querySelector('p').innerHTML = review.review;
-  // there will always be 1 star because it is a part of the template
-  for (let i = 1; i < review.rating; ++i) {
-    const img = tmpl.querySelector('img').cloneNode();
-    tmpl.querySelector('.rating').appendChild(img);
+  tmpl.querySelector('h4').innerText = review.reviewer;   // add content to
+  tmpl.querySelector('h3').innerText = review.title;      // the elements
+  tmpl.querySelector('p').innerText = review.review;      // in the template
+  // there will always be 1 star because it is a part of the template so we start for loop at 1 stars
+  for (let i = 1; i < review.rating; ++i) {   // add stars if the rating was greater than 1
+    const img = tmpl.querySelector('img').cloneNode();  // Make a copy of the img element
+    tmpl.querySelector('.rating').appendChild(img);       // add new img element to rating div
   }
-  main.appendChild(tmpl);
+  main.appendChild(tmpl);   // Add the template to the DOM
 }
 
 // LECTURE STARTS HERE ---------------------------------------------------------------
+//            These functions will execute when the page is loaded into the browser
+
+//            Since these functions contain statements that manipulate the DOM
+//              if the DOM has not yet been created when they run, they will fail
+
+//            So we should wait until the DOM is created before allowing them to run
+//            Luckily, the browser will tell us when it has finished creating the DOM
+//              by firing the DOMContentLoaded event
+
+//    Wait until the DOM is loaded/created before manipulating the DOM with these functions
+//    So we're adding an event handler to the document to be run when the DOMContentLoaded event happens
+//      .addEventListener(event-to-listen-for, function-to-handle-event)
+document.addEventListener('DOMContentLoaded', () => {
 
 // set the product reviews page title
-setPageTitle();
+                                                      setPageTitle();
 // set the product reviews page description
-setPageDescription();
+                                                      setPageDescription();
 // display all of the product reviews on our page
-displayReviews();
+                                                      displayReviews();
+
 
 /**
  * Take an event on the description and swap out the description for a text box.
  *
  * @param {Event} event the event object
  */
+
+const descriptionLine = document.querySelector('.description');    // Get a reference to element we want the Event handled
+
+// Add an event handler for the click event ot the description line using an anon-func which will call
+//    the function provided to us by passing it a reference to the element clicked (event.target)
+// event.target - get the target of the event from the event object (the element that was clicked in this case)
+descriptionLine.addEventListener('click', (anEvent) => {    // an anon-func that takes the event object as a parameter
+  toggleDescriptionEdit(anEvent.target)
+});
+
+
+// Add an eventhandler to the input box to accept the input and change the description when they press enter
+
+const theInputBox = document.getElementById('inputDesc'); // Get a reference to the input box
+theInputBox.addEventListener('keyup', (someEvent) => {
+
+  if (someEvent.key === 'Enter') {    // checking if they press the enter key, then do X
+    exitDescriptionEdit(someEvent.target, true);   // run the function provided to accept the input and set the description
+  }  
+
+  if (someEvent.key === 'Escape') {    // checking if they press the escape key, then do X
+    exitDescriptionEdit(someEvent.target, false);   // run the function provided to decline the input and set the description
+  }  
+});
+
+
+// Add an event listener to the button for 'Add a Review'
+const theAddReviewButton = document.getElementById('btnToggleForm');
+theAddReviewButton.addEventListener('click', (doThisEvent) => {
+  showHideForm();   // Run the function to show or hide the form
+});
+
+
+// Add an event listener for the 'saveReview' button that will run the function SaveReview
+const theSaveReviewButton = document.getElementById('btnSaveReview');
+theSaveReviewButton.addEventListener('click', (justAnotherEvent) => {
+  // Due to this being a submit button for a form - we do not want to go to the server with an HTTP request
+  // We want to handle the button processing
+  justAnotherEvent.preventDefault();    // Tell the button to not perform its default behavior to submit to API
+  saveReview();   // Run the function to show or hide the form
+});
+
+
+});   // End of the event handling code from DOMContentLoadedEvent
+
+
+
+// @param { element-clicked} the element the user clicked which caused this function to run
+
+
+
+
 function toggleDescriptionEdit(desc) {
-  const textBox = desc.nextElementSibling;
-  textBox.value = description;
-  textBox.classList.remove('d-none');
-  desc.classList.add('d-none');
-  textBox.focus();
+  const textBox = desc.nextElementSibling;    // Get a reference to the next sibling of the element that was clicked
+                                              //        allow us to access the input element following the description
+  textBox.value = description;            // Set the initial value of the input box to the default description
+ //   .classlist represents the list of classes currently assigned to element - use it to add/remove move classes
+  textBox.classList.remove('d-none');     // un-hide the input box by removing the d-none class
+  desc.classList.add('d-none');           //  hide the element that was clicked by adding the d-none class
+  textBox.focus();                        // put the cursor (focus) in the input box
 }
 
 /**
@@ -87,30 +159,31 @@ function toggleDescriptionEdit(desc) {
  * @param {Event} event the event object
  * @param {Boolean} save should we save the description text
  */
-function exitDescriptionEdit(textBox, save) {
-  let desc = textBox.previousElementSibling;
-  if (save) {
-    desc.innerText = textBox.value;
+function exitDescriptionEdit(textBox, save) {   // receive the element where Enter or Escape was pressed (currently on line 109 and 113)
+                                                // The second parameter is a boolean indicate should the content be save
+  let desc = textBox.previousElementSibling;    // Get a reference to element that is a previous sibling to this one (description line)
+  if (save) {                             // save was requested (true)
+    desc.innerText = textBox.value;       //    then set the description content to the content fo the input box
   }
-  textBox.classList.add('d-none');
-  desc.classList.remove('d-none');
+  textBox.classList.add('d-none');        // hide the input box (add d-none to its class)
+  desc.classList.remove('d-none');        // display the description box (remove d-none from class)
 }
 
 /**
  * I will show / hide the add review form
  */
-function showHideForm() {
-  const form = document.querySelector('form');
-  const btn = document.getElementById('btnToggleForm');
+function showHideForm() {                               // This will hide or show the form for a review
+  const form = document.querySelector('form');          //    Get a reference to the 'form' in the html
+  const btn = document.getElementById('btnToggleForm'); //    Get a reference to the 'Add Review' button in the html
 
-  if (form.classList.contains('d-none')) {
-    form.classList.remove('d-none');
-    btn.innerText = 'Hide Form';
-    document.getElementById('name').focus();
-  } else {
-    resetFormValues();
-    form.classList.add('d-none');
-    btn.innerText = 'Add Review';
+  if (form.classList.contains('d-none')) {        // If the form is currently hidden (class has a d-none)
+    form.classList.remove('d-none');              //      Remove the d-none to display the form
+    btn.innerText = 'Hide Form';                  //      Change button to say 'Hide Form'
+    document.getElementById('name').focus();      //      Find the element with the id= 'name' and put the cursor in it
+  } else {                                              // Else if the form is currently displayed
+    resetFormValues();                                  //    Reset the form values using function provided
+    form.classList.add('d-none');                       //    Add the d-none class to the form to hide it
+    btn.innerText = 'Add Review';                       //    Make the button say 'Add Review'
   }
 }
 
@@ -130,4 +203,27 @@ function resetFormValues() {
 /**
  * I will save the review that was added using the add review from
  */
-function saveReview() {}
+function saveReview() {
+  // We need to add an element to our reviews array with the values from the form
+  // Each array element is a Javascript object
+
+
+  // Get a reference to input box in the form
+  const nameBox = document.getElementById('name');
+  const titleBox = document.getElementById('title');
+  const reviewBox = document.getElementById('review');
+  const ratingBox = document.getElementById('rating');
+
+  // Get the values from the form and create a new element to add to the reviews array
+  const newReview = {
+    reviewer: nameBox.value,   // create a reviewer in the new review with the value from 'name' input box
+    title: titleBox.value,   // create a title in the new review with the value from 'title' input box
+    review: reviewBox.value,   // create a review in the new review with the value from 'review' input box
+    rating: ratingBox.value   // create a rating in the new review with the value from 'rating' input box
+  }
+
+  // Add the new review object to the reviews array
+  reviews.push(newReview);      // Add the new review to the array in memory
+  displayReview(newReview);     // Add the new review to the DOM/html
+  showHideForm();   // Run the function to show or hide the form
+}
