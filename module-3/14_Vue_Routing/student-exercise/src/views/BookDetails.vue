d431d1ca1b58841810c5aa47ac0f4fb522925c5f
Brian Kilburn <briankilburn93@gmail.com>
2020-12-02 00:20:40 -0500

    current HW push

---
+++ module-3/14_Vue_Routing/student-exercise/src/views/BookDetails.vue
@@ -0,0 +1,32 @@
+<template>
+    
+<div>
+    <book-details />
+</div>
+
+</template>
+
+<script>
+import BookDetails from '@/components/BookDetails.vue';
+
+export default {
+    components: {
+        BookDetails
+    },
+    created() {
+        const currentProductId = this.$route.params.isbn;
+        this.$store.commit("SET_ACTIVE_PRODUCT", currentISBN);
+    },
+    computed: {
+        product() {
+            return this.$store.state.books.find(
+                (aProduct) => { return aProduct.isbn == this.$store.state.activeProduct;})
+        }
+    }
+    
+}
+</script>
+
+<style scoped> /* CSS */
+
+</style>
---
+++ module-3/14_Vue_Routing/student-exercise/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    
+<div>
+    <best-seller-list />
+    <new-releases-list />
+
+    <div id="nav">
+        <router-link to="/newBook">Add Book to Reading List</router-link>
+    </div>
+</div>
+
+</template>
+
+<script>
+import BestSellerList from '@/components/BestSellerList.vue';
+import NewReleasesList from '@/components/NewReleasesList.vue';
+
+export default {
+    components: {
+        BestSellerList,
+        NewReleasesList
+    }
+    
+}
+</script>
+
+<style scoped> /* CSS */
+
+</style>
